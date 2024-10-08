b77982819f401b26b1df8c6b1fbb9ee13baebbab
Srishti1622 <shrishtibindki@gmail.com>
2024-10-08 22:57:27 +0530

    added main.py

---
+++ Flask Framework/main.py
@@ -0,0 +1,8 @@
+# Reference: https://flask.palletsprojects.com/en/3.0.x/quickstart/
+# Flask is used to develop end-to-end web applications.It is a web framework which is created with the help of python programming
+# Two important components: WSGI ( Web Server Gateway Interface ) and Jinja2 Template Engine
+
+# WSGI
+
+# Web Server (AWS,Azure,Apache,IIS)  <---->  Web Application (created with the help of flask)
+# The protocol used to redirect the request and get the response 
