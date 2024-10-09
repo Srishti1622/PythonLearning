50ffb9a9099594dc0fc6ee5e3f41452a41a2db20
Srishti1622 <shrishtibindki@gmail.com>
2024-10-09 15:46:08 +0530

    intro flask

--- Flask Framework/main.py
+++ Flask Framework/main.py
@@ -4,5 +4,9 @@
 
 # WSGI
 
-# Web Server (AWS,Azure,Apache,IIS)  <---->  Web Application (created with the help of flask)
-# The protocol used to redirect the request and get the response 
+# Web Server (AWS,Azure,Apache,IIS)  <----WSGI---->  Web Application (created with the help of flask)
+# The WSGI protocol used to redirect the request and get the response from the web application 
+
+# Jinja2 Template Engine
+
+# It is a Web Template Engine which is used to combine the layout of the page with a data source ( can be SQL DB, CSV sheet, ML model, Mongodb ) which will help to load or create dynamic the web page.
