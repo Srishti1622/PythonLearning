1146392f88a06e3a0c747496a3e14e7b29c2d408
Srishti1622 <shrishtibindki@gmail.com>
2024-10-12 12:52:59 +0530

    intro fastapi

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -13,3 +13,33 @@
 # Flexible: ASGI provides greater flexibility by allowing both blocking (synchronous) and non-blocking (asynchronous) code in the same application. Frameworks like FastAPI and newer versions of Django (with Daphne as the ASGI server) support ASGI.
 # Better for real-time applications: If you need to handle real-time updates, WebSockets, or background tasks, ASGI is the better choice.
 
+# To run the FastAPI code, we need to run the command-
+# uvicorn filename:object(instance name) --reload
+# in this case, uvicorn main:app --reload
+
+# It is neccesary to import uvicorn as we will be mentioning that it has to follow ASGI Interface
+import uvicorn
+from fastapi import FastAPI
+
+# creating an instance of fastAPI
+app = FastAPI()
+
+# there is one url as http://localhost:8000/docs
+# It will display the complete code in ui for testing our written APIs 
+# similar we have /redoc
+
+# Index route, opens automatically on http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'messgae':'Hello, API'}
+
+# this url will get from /docs
+# url will be like http://locahost:8000/Welcome?name=name
+@app.get('/Welcome')
+def welcome(name:str):
+    return {'message':f'hello, {name}'}
+
+# entry point of code, run the api with uvicorn
+# will run on http://127.0.0.1:8000
+if __name__=="__main__":
+    uvicorn.run(app,host='127.0.0.1',port=800)
