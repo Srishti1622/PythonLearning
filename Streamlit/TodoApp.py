106d29aa43bc43190ef8f1e1031f8d64dd4acecf
Srishti1622 <shrishtibindki@gmail.com>
2024-10-13 12:33:24 +0530

    updated todoapp

--- Streamlit/TodoApp.py
+++ Streamlit/TodoApp.py
@@ -17,15 +17,30 @@ st.title("To-Do-App")
 
 def showTask():
     cursor.execute("select * from todoApp")
+    return cursor.fetchall()
 
 def addTask(name,status):
     cursor.execute("insert into todoApp(name,status) values(?,?)",(name,status))
+    task=showTask()
+    for t in task:
+        print(t)
+    connection.commit()
 
 def updateTask():
     cursor.execute("update todoApp set ")
 
-task_name=st.text_input("Enter task name here",key="")
+task_name=st.text_input("Enter task name here")
 task_status=st.text_input("Enter task status here")
 
 if st.button("Add task"):
-    if task_name:
+    if not task_name:
+        st.warning("Please enter task name")
+    else:
+        addTask(task_name,task_status)
+        st.success("Task added successfully!")
+
+print("show list yrr")
+if st.button("Show task list"):
+    taskList=showTask()
+    for task in taskList:
+        print(task)
