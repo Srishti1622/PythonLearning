04a50aac676bcef65eb58237a8579695b1938574
Srishti1622 <shrishtibindki@gmail.com>
2024-10-14 18:17:04 +0530

    completed todoapp using streamlit and sqlite3

--- Streamlit/TodoApp.py
+++ Streamlit/TodoApp.py
@@ -14,6 +14,7 @@ cursor.execute("""
 connection.commit()
 
 st.title("To-Do-App")
+st.sidebar.title("Add your task here")
 
 def showTask():
     cursor.execute("select * from todoApp")
@@ -21,26 +22,50 @@ def showTask():
 
 def addTask(name,status):
     cursor.execute("insert into todoApp(name,status) values(?,?)",(name,status))
-    task=showTask()
-    for t in task:
-        print(t)
     connection.commit()
 
-def updateTask():
-    cursor.execute("update todoApp set ")
+def updateTask(task_id,name,status):
+    cursor.execute("update todoApp set name=?, status=? where id=?",(name,status,task_id))
+    connection.commit()
 
-task_name=st.text_input("Enter task name here")
-task_status=st.text_input("Enter task status here")
+def deleteTask(task_id):
+    cursor.execute("delete from todoApp where id=?",(task_id,))
+    connection.commit()
 
-if st.button("Add task"):
+task_name=st.sidebar.text_input("Enter task name here")
+task_status=st.sidebar.text_input("Enter task status here")
+
+if st.sidebar.button("Add task"):
     if not task_name:
-        st.warning("Please enter task name")
+        st.sidebar.warning("Please enter task name")
     else:
         addTask(task_name,task_status)
-        st.success("Task added successfully!")
+        st.sidebar.success("Task added successfully!")
 
-print("show list yrr")
-if st.button("Show task list"):
-    taskList=showTask()
+taskList=showTask()
+if not taskList:
+    st.write("No task to show.")
+else:
+    st.write("List of task : ")
     for task in taskList:
-        print(task)
+        col1, col11, col2, col3 = st.columns([4, 4, 2, 2]) 
+        with col1:
+            st.write(f"**Task Name:** {task[1]}")
+
+        with col11:
+            st.write(f"**Task Status:** {task[2]}")
+
+        with col2:
+            if st.button("Update",key=f"update_button_{task[0]}"):
+                if not task_name:
+                    st.sidebar.warning("Please enter task name")
+                else:
+                    updateTask(task[0],task_name,task_status)
+                    st.sidebar.success("Task updated successfully!")
+
+        with col3:
+            if st.button("Delete",key=f"delete_button_{task[0]}"):
+                deleteTask(task[0])
+                st.sidebar.success("Task deleted successfully!")
+
+connection.close()
