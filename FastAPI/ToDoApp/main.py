4d893bdcefddbe592924b41c422380151312eb4b
Srishti1622 <shrishtibindki@gmail.com>
2024-11-09 14:01:06 +0530

    slqite3 in terminal

--- FastAPI/ToDoApp/main.py
+++ FastAPI/ToDoApp/main.py
@@ -10,3 +10,15 @@ app=FastAPI()
 # this will create a database in this mentioned location in database.py in the variable SQLALCHEMY_DATABASE_URL
 # in this case it's inside ToDoApp foldera
 models.Base.metadata.create_all(bind=engine)
+
+
+
+# If sqlite3 is installed then we make use of terminal to manipulate database 
+# it will open sqlite environment
+# -- sqlite3 database_name.db here sqlite3 todos.db
+# it will show the schema of the table
+# -- .schema
+# run sql queries to manipulate the table
+# -- insert into todos (title,description,priority,complete) values ('go to store', 'pick the eggs', 5, False)
+# to display the columns/table in terminal in better format use 
+# -- .mode column, .mode markdown, .mmode box
