30fb2fb7272321201c8af3da54241c360750ff30
Srishti1622 <shrishtibindki@gmail.com>
2024-11-09 13:04:21 +0530

    slqalchemy connection

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 # this url is going to used to create a location of this database on our fastapi application
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
@@ -10,3 +12,9 @@ SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 # but in fastAPI it's very normal to have more than one thread that colud interact with the database at the same time
 # so to make sure SQLite knows that we don't want to check the same thread all the time because there colud be multiple threads happening to our SQLite database by mentioning 'check_same_thread=False'
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+# this is the session local, which we are going to be using in our application, we want to bind to the engine that we created and we want to make sure that our auto commits and auto flushes are false or the database transactions are going to try and do something automatically and we want to be fully control of everything our database will do in the future
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# create a base which is an object of the database which is going to be able to control our database
+Base=declarative_base()
