c76a71f93930fd001f8e9958a3a37dc817700e29
Srishti1622 <shrishtibindki@gmail.com>
2024-11-09 15:30:08 +0530

    delete api in todoapp

--- FastAPI/ToDoApp/main.py
+++ FastAPI/ToDoApp/main.py
@@ -62,8 +62,8 @@ def create_todo(db: db_dependency, todo: ToDoRequest):
     db.add(todo_model)
     db.commit()
 
-@app.put('/todo/update/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
-def update_specific_todo(db: db_dependency, todo_id: int , todo: ToDoRequest):
+@app.put('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
+def update_specific_todo(db: db_dependency, todo: ToDoRequest, todo_id: int = Path(gt=0)):
     todo_model=db.query(Todos).filter(Todos.id==todo_id).first()
     if todo_model is None:
         raise HTTPException(status_code=404, detail='Todo not found')
@@ -74,4 +74,13 @@ def update_specific_todo(db: db_dependency, todo_id: int , todo: ToDoRequest):
     todo_model.complete=todo.complete
 
     db.add(todo_model)
+    db.commit()
+
+@app.delete('/todo/{todo_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_specific_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model=db.query(Todos).filter(Todos.id==todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not found')
+
+    db.query(Todos).filter(Todos.id==todo_id).delete()
     db.commit()
