c24d09970597117e42f5e85ac244eaaf8e54614f
Srishti1622 <shrishtibindki@gmail.com>
2024-11-23 21:12:37 +0530

    connecting to sqlit3 and mysql

---
+++ LangChain/projects/ChatWithSQLdb/app.py
@@ -0,0 +1,69 @@
+# An application in which we are connect and querying both local db as well as mysql database 
+
+import streamlit as st
+from pathlib import Path
+from sqlalchemy import create_engine
+import sqlite3
+# Construct a SQL agent from an LLM and toolkit or database
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.sql_database import SQLDatabase
+from langchain.agents.agent_types import AgentType
+from langchain_groq import ChatGroq
+
+
+st.set_page_config(page_title="LangChain: Chat with SQL DB")
+st.title("Chat with SQL DB")
+
+LOCALDB='USE_LOCALDB'
+MYSQL='USE_MYSQL'
+
+radio_opt=['Use SQLite3', 'Use MYSQL']
+selected_opt=st.sidebar.radio(label="Choose the DB to chat",options=radio_opt)
+
+if radio_opt.index(selected_opt)==1:
+    db_uri=MYSQL
+    mysql_host=st.sidebar.text_input("Enter Host")
+    mysql_user=st.sidebar.text_input("Enter User")
+    mysql_password=st.sidebar.text_input("Enter Password",type="password")
+    mysql_db=st.sidebar.text_input("Enter Database")
+else:
+    db_uri=LOCALDB
+
+
+api_key=st.sidebar.text_input("Enter Groq API Key",type="password")
+
+if not db_uri:
+    st.info("Please provide the database info")
+
+if not api_key:
+    st.info("Please provide api key")
+
+# creating llm model
+groq_llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
+
+# this decorator is for caching as we don't need to connect each time to db
+# ttl= total time limit
+@st.cache_resource(ttl='2h')
+def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
+    if db_uri==LOCALDB:
+        # setup the filepath to local db here 'student.db'
+        db_filepath=(Path(__file__).parent/"student.db").absolute()
+        print(db_filepath)
+        # creating a creator to connect to sqlite database in read only mode
+        creator=lambda: sqlite3.connect(f"file:{db_filepath}?mode=ro",uri=True)
+        return SQLDatabase(create_engine("sqlite:///", creator=creator))
+    elif db_uri==MYSQL:
+        if not (mysql_host and mysql_user and mysql_password and mysql_db):
+            st.error("Please provide all MYSQL details")
+            st.stop()
+        # this is how we connect to MYSQL database
+        return SQLDatabase(create_engine(f"mysql+mysqlconnector://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_db}"))
+    
+
+if db_uri==MYSQL:
+    db=configure_db(db_uri,mysql_host,mysql_user,mysql_password,mysql_db)
+else:
+    db=configure_db(db_uri)
+
