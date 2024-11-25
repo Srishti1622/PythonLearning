fc0c7ec671fc12a4076c3ed366e3ae724f41a82b
Srishti1622 <shrishtibindki@gmail.com>
2024-11-25 22:22:10 +0530

    getting error with youtube urls

--- LangChain/projects/summarizeText.py
+++ LangChain/projects/summarizeText.py
@@ -11,7 +11,10 @@ from langchain.prompts import PromptTemplate
 from langchain_groq import ChatGroq
 from langchain.chains.summarize import load_summarize_chain
 from langchain_community.document_loaders import YoutubeLoader,UnstructuredURLLoader
-
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+from pytube import YouTube
+from langchain_core.documents import Document
 
 # streamlit app
 st.set_page_config(page_title="Summarize text from Youtube or Website")
@@ -34,6 +37,16 @@ prompt=PromptTemplate(
     template=prompt_template
 )
 
+# Function to fetch transcript
+def fetch_youtube_transcript(video_url):
+    video_id = video_url.split("v=")[-1]
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        formatter = TextFormatter()
+        return formatter.format_transcript(transcript)
+    except Exception as e:
+        return f"Error fetching transcript: {e}"
+
 # click button to get the summarize content
 if st.button("Summarize"):
     if not api_key.strip() or not url.strip():
@@ -48,19 +61,31 @@ if st.button("Summarize"):
 
                 # for loading data from youtube url
                 if "youtube.com" in url:
-                    loader=YoutubeLoader.from_youtube_url(url,add_video_info=True)
+                    # loader=YoutubeLoader.from_youtube_url(url,add_video_info=True)
+                    transcript_text = fetch_youtube_transcript(url)
+                    if transcript_text.startswith("Error"):
+                        st.error(transcript_text)
+                    else:
+                        # docs = [{"page_content": transcript_text}]
+                        docs=Document(
+                            page_content=transcript_text,
+                            metadata={"source": url}
+                        )
                 else:
                     loader=UnstructuredURLLoader(
                         urls=[url],
                         ssl_verify=False,
                         headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 13_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
                     )
-                docs=loader.load()
+                    docs=loader.load()
+                
+                st.write(docs)
 
                 # chain for summarization
                 chain=load_summarize_chain(llm=groq_llm,chain_type="stuff",prompt=prompt)
                 output_summary=chain.run(docs)
 
+                st.write("reached")
                 st.success(output_summary)
         
         except Exception as e:
