291a4b14f670b1c1b4cec014bd4e397c089d1abc
Srishti1622 <shrishtibindki@gmail.com>
2024-12-02 20:49:33 +0530

    setting up the llm model

--- Google Gemini/ChatText.py
+++ Google Gemini/ChatText.py
@@ -5,3 +5,17 @@ import streamlit
 import os
 import google.generativeai as genai
 
+# loading the environment variables
+load_dotenv()
+
+# setting up the api key
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# creating llm model
+model=genai.GenerativeModel("gemini-pro")
+
+# function to call llm model and get the response based on passed question
+def get_response(question):
+    # this .generate_content() method is used to pass the input to llm model
+    response=model.generate_content(question)
+    return response
