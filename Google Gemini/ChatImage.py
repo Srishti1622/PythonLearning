5b33a6d7e7cc210e633ed0b6aa718c832ccc4bfe
Srishti1622 <shrishtibindki@gmail.com>
2024-12-02 21:44:02 +0530

    chat with image app done

--- Google Gemini/ChatImage.py
+++ Google Gemini/ChatImage.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import streamlit as st
 import os
 import google.generativeai as genai
+from PIL import Image
 
 # loading the environment variables
 load_dotenv()
@@ -12,7 +13,10 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # creating llm model
-model=genai.GenerativeModel("gemini-pro-vision")
+# "gemini-pro-vision" is not present now 
+# model=genai.GenerativeModel("gemini-pro-vision")
+# "gemini-1.5-flash" is take both text and image
+model=genai.GenerativeModel("gemini-1.5-flash")
 
 # function to call llm model and get the response based on passed question
 def get_response(input,image):
@@ -22,3 +26,25 @@ def get_response(input,image):
     else:
         response=model.generate_content(image)
     return response.text
+
+# streamlit setup
+st.set_page_config(page_title="Gemini Image Demo")
+
+st.header("Google Gemini Image Application")
+
+input=st.text_input("Ask your question")
+
+# getting image from user
+uploaded_image=st.file_uploader("Choose an image...",type=['jpg','jpeg','png'])
+image=''
+if uploaded_image is not None:
+    image=Image.open(uploaded_image)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+ask=st.button("Tell me about image")
+
+if ask and not image:
+    st.warning("Please provide image!")
+
+if ask and image:
+    response=get_response(input,image)
+    st.write(response)
