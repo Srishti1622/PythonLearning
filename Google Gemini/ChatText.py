ec49864b93e72bb410133587e392c93f0120e363
Srishti1622 <shrishtibindki@gmail.com>
2024-12-02 20:44:50 +0530

    requirements

---
+++ Google Gemini/ChatText.py
@@ -0,0 +1,7 @@
+# Google Gemini for text based chatbot where user can ask any question and get the response
+
+from dotenv import load_dotenv
+import streamlit
+import os
+import google.generativeai as genai
+
