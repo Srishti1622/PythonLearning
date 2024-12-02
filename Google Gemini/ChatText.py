50811a70c89c131bbd0ef2d06067682a4553b5a0
Srishti1622 <shrishtibindki@gmail.com>
2024-12-02 21:09:05 +0530

    qna app done

--- Google Gemini/ChatText.py
+++ Google Gemini/ChatText.py
@@ -1,7 +1,7 @@
 # Google Gemini for text based chatbot where user can ask any question and get the response
 
 from dotenv import load_dotenv
-import streamlit
+import streamlit as st
 import os
 import google.generativeai as genai
 
@@ -19,3 +19,18 @@ def get_response(question):
     # this .generate_content() method is used to pass the input to llm model
     response=model.generate_content(question)
     return response
+
+# streamlit setup
+st.set_page_config(page_title="QnA Demo")
+
+st.header("Google Gemini Text Application")
+
+input=st.text_input("Ask your question")
+ask=st.button("Ask")
+
+if ask and not input:
+    st.warning("Please enter your question!")
+
+if ask and input:
+    response=get_response(input)
+    st.write(response.text)
